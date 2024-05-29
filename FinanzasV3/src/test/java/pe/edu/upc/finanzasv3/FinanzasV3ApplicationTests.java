5ba1d4c99c0999df9d7db531e7d166c6f35cfa9d
Fabrizio Hidalgo <109826993+HidalgoFabrizio7@users.noreply.github.com>
2024-05-29 11:12:54 -0500

    add V3

---
+++ FinanzasV3/src/test/java/pe/edu/upc/finanzasv3/FinanzasV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.finanzasv3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanzasV3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
