0f1264876e86ffb1c01bccb7da3c5f106d53ef71
Fabrizio Hidalgo <academyfabrizio7@gmail.com>
2024-06-15 19:05:04 -0500

    correcion seguridad

--- src/main/java/pe/edu/upc/finanzasv3/FinanzasV3Application.java
+++ src/main/java/pe/edu/upc/finanzasv3/FinanzasV3Application.java
@@ -1,9 +1,12 @@
 package pe.edu.upc.finanzasv3;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@SecurityScheme(name = "Bearer Authentication", type = SecuritySchemeType.APIKEY, bearerFormat = "JWT", scheme = "bearer")
 public class FinanzasV3Application {
 
     public static void main(String[] args) {
